d602cf49560589a107907d5d40cb6ef8b32faa55
Alisa Prathnadi <aprathna@ucsd.edu>
2016-02-06 00:50:17 +0000

    putting it together lab

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,14 @@
 var data = require("../data.json");
 
 exports.addFriend = function(req, res) {    
-	// Your code goes here
+	// Your code goes heexports.addFriend = function(req, res) {
+
+	var newFriend = {
+		"name": req.query.name,
+		"description": req.query.description,
+		"imageURL": "http://lorempixel.com/400/400"
+	}
+	console.log(newFriend);
+	res.render('add', newFriend);
+	data["friends"].push(newFriend);
  }
